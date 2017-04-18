cc0b109557e78f5579d5cbd33987d3c4cce844e1
ytrofy <navy.trofy@gmail.com>
2017-04-18 09:59:36 -0700

    Update Arrays_Handout.java

--- Arrays_Handout.java
+++ Arrays_Handout.java
@@ -31,6 +31,7 @@
 *******************************************************************************
 *******************************************************************************
 ******************************************************************************/
+import java.awt.*;
 
 public class Arrays_Handout
 {
@@ -39,5 +40,22 @@ public class Arrays_Handout
       //Initialize array and pass it to a method to display it.
       
       //Have a method to fill an array and return it to main, then pass to display method.
+     
+	   
+	// what yvans wants to do, the very very basics
+	str[1] = "Is";
+	num[0] = 100;
+	num[1] = 200;
+	
+	   
+	 String[] str = {"Java","Isn't","Awesome"};
+	
+	int[] num = new int[3]
+        
+	JOptionPane.showMessageDialog(null,
+				      "The string list is " + str.length + " words long \n"    // should say 3
+				     +"The integer list is " + num.length + "numbers long \n"  // should say 3
+				     + num[0] + "," +num[1] + "," +num[2] +"\n"  //should say 100, 200, 0
+				     + str[0] + str[1] + str[2]);  //shoud say java is awesome
    }
 }
